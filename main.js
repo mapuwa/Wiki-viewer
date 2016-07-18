e5961f0d8ba61ce2eb6c903e796e006944c791df
Mi Mapuwa <mapuwa@protonmail.com>
2016-07-18 10:42:46 +0200

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+/**
+ * Wikipedia API queries
+ * ######################
+ *
+ * Search
+ * ======
+ * https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=php
+ * Array- first item = query
+ *        second item = results names
+ *        third item = results descriptions
+ *        fourth item = results links
+ *
+ * Random page
+ * ===========
+ * https://en.wikipedia.org/wiki/Special:Random
+ */
